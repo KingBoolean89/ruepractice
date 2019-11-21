0759199c931589e095c7d2d4abfb9a90119cfa40
KingBoolean89 <aking893.ak@gmail.com>
2019-11-21 09:29:15 -0500

    adding views and resources

--- rue21/parse/views.py
+++ rue21/parse/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from tablib import Dataset
 
-# Create your views here.
+def simple_upload(request):
+    if request.method == 'POST':
+        product_resource = PersonResource()
+        dataset = Dataset()
+        new_product = request.FILES['myfile']
+
+        imported_data = dataset.load(new_product.read())
+        result = product_resource.import_data(dataset, dry_run=True)  # Test the data import
+
+        if not result.has_errors():
+            product_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+    return render(request, 'core/simple_upload.html')
+
+
